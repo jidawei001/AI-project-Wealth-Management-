6f6329b82b54b5fb3c943826b57e41fb7dda24ca
SDW1222 <shidawei1222@hotmail.com>
2024-09-30 04:31:32 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask,render_template,request
+import google.generativeai as genai
+import os
+import numpy as np
+from textblob import TextBlob
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key="AIzaSyCluRjLkwn6IOe4f-dNTlusyuAkDUI7fQo")
+
+app = Flask(__name__)
+user_name = ""
+flag = 1
 
